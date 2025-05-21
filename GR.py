c7ea0494427748d22e058c30411afdd7ef590d0c
daf201 <daf201@blink-in.com>
2025-05-20 17:22:31 -0700

    Add files via upload

--- GR.py
+++ GR.py
@@ -15,43 +15,43 @@ def build_GR() -> None:
     if GR_file_path == "":
         return
 
-    GR_FILE_BASE_DIRECTORY = GR_file_path.replace(
+    gr_file_base_dir = GR_file_path.replace(
         path.basename(GR_file_path), '')
 
-    GR_FILE = win32com.client.Dispatch("Excel.Application")
-    GR_FILE_WORKBOOK = GR_FILE.Workbooks.Open(GR_file_path)
+    gr_file = win32com.client.Dispatch("Excel.Application")
+    gr_file_workbook = gr_file.Workbooks.Open(GR_file_path)
 
     # for easier access
-    GR_FILE_SHEET1 = GR_FILE_WORKBOOK.Sheets(1)
-    GR_FILE_SHEET2 = GR_FILE_WORKBOOK.Sheets(2)
-    GR_FILE_SHEET3 = GR_FILE_WORKBOOK.Sheets(3)
+    gr_file_sheet1 = gr_file_workbook.Sheets(1)
+    gr_file_sheet2 = gr_file_workbook.Sheets(2)
+    gr_file_sheet3 = gr_file_workbook.Sheets(3)
 
     # the PB Number of this GR
-    GR_PB = GR_FILE_SHEET2.Cells(2, 1).Value
+    GR_PB = gr_file_sheet2.Cells(2, 1).Value
 
     # get the starting sn number index, place holder
     sn_start = 0
 
     # get range of sn
-    sn_size = int(GR_FILE_SHEET2.Cells(2, 4))
+    sn_size = int(gr_file_sheet2.Cells(2, 4))
 
     # sn end, which is the last sn of this wo, all sn should be with in, let say 100 units, start 01, the max is 100, so 01+100-1 is the max sn
     sn_end = sn_start+sn_size-1
 
     # if this file is a feed file, then need to create other 2 sheets
-    if GR_FILE_WORKBOOK.Sheets.Count == 1:
+    if gr_file_workbook.Sheets.Count == 1:
 
         message(__name__, "FEEDFILE DETECTED, START BUILDING GR FILE FROM FEEDFILE")
 
         # create gr copy sheet and SN STT sheet
-        GR_FILE_WORKBOOK.Sheets.Add(
-            Before=GR_FILE_SHEET1).Name = "Sheet 2"
+        gr_file_workbook.Sheets.Add(
+            Before=gr_file_sheet1).Name = "Sheet 2"
 
-        GR_FILE_WORKBOOK.Sheets.Add(
-            After=GR_FILE_SHEET2).Name = "Sheet 1"
+        gr_file_workbook.Sheets.Add(
+            After=gr_file_sheet2).Name = "Sheet 1"
 
         # move the original sheet to index 2
-        GR_FILE_SHEET3.Move(Before=GR_FILE_SHEET2)
+        gr_file_sheet3.Move(Before=gr_file_sheet2)
 
         message(__name__, "COLLECTING INFOMATION")
         # get SN start until get a valid sn or quit command
@@ -80,37 +80,37 @@ def build_GR() -> None:
         for row_num in range(2, sn_size+2):
 
             # set the SN to start + displacement(row_num-2 since row start from 1 and row 1 is useless)
-            GR_FILE_SHEET2.Cells(
+            gr_file_sheet2.Cells(
                 row_num, 5).Value = sn_start+row_num-2
 
             # set to number, no digital
-            GR_FILE_SHEET2.Cells(
+            gr_file_sheet2.Cells(
                 row_num, 5).NumberFormat = "0"
 
             # set to blue
-            GR_FILE_SHEET2.Cells(
+            gr_file_sheet2.Cells(
                 row_num, 5).Font.Color = 16711680
 
             # put a PASS at the cordinating cell
-            GR_FILE_SHEET2.Cells(
+            gr_file_sheet2.Cells(
                 row_num, 12).Value = "PASS"
 
         # save the GR file
-        GR_FILE_WORKBOOK.Save()
+        gr_file_workbook.Save()
         message(__name__, "GR FILE BUILT")
 
     # now the GR file is ready, need to file data
 
     # get range of the sn (because this may also be a GR file, which does not knowns range)
-    sn_start = int(GR_FILE_SHEET2.Cells(2, 5).Value)
-    sn_end = sn_start+int(GR_FILE_SHEET2.Cells(2, 4).Value)-1
+    sn_start = int(gr_file_sheet2.Cells(2, 5).Value)
+    sn_end = sn_start+int(gr_file_sheet2.Cells(2, 4).Value)-1
 
     # remove the sheet 3
-    GR_FILE_SHEET3.Cells.Clear()
+    gr_file_sheet3.Cells.Clear()
 
     # put SN and STT at sheet 3
-    GR_FILE_SHEET3.Cells(1, 1).Value = "SN"
-    GR_FILE_SHEET3.Cells(1, 2).Value = "STT"
+    gr_file_sheet3.Cells(1, 1).Value = "SN"
+    gr_file_sheet3.Cells(1, 2).Value = "STT"
 
     message(__name__, "START SCANNING")
 
@@ -158,20 +158,20 @@ def build_GR() -> None:
     # fill the sheet 3
     row_num = 2
     for sn in sn_list:
-        GR_FILE_SHEET3.Cells(row_num, 1).Value = sn
-        GR_FILE_SHEET3.Cells(row_num, 1).NumberFormat = "0"
-        GR_FILE_SHEET3.Cells(row_num, 2).Value = "PASS"
+        gr_file_sheet3.Cells(row_num, 1).Value = sn
+        gr_file_sheet3.Cells(row_num, 1).NumberFormat = "0"
+        gr_file_sheet3.Cells(row_num, 2).Value = "PASS"
         row_num += 1
 
     # remove sheet1, copy title from sheet2 to 1
-    GR_FILE_SHEET1.Cells.Clear()
-    sn_col = GR_FILE_SHEET2.Columns(5)
+    gr_file_sheet1.Cells.Clear()
+    sn_col = gr_file_sheet2.Columns(5)
 
-    GR_FILE_SHEET2.Rows(1).Copy()
-    GR_FILE_SHEET1.Rows(1).PasteSpecial(Paste=-4163)
+    gr_file_sheet2.Rows(1).Copy()
+    gr_file_sheet1.Rows(1).PasteSpecial(Paste=-4163)
 
     # show all data for searching
-    GR_FILE_SHEET2.ShowAllData()
+    gr_file_sheet2.ShowAllData()
 
     # start from row 2
     sheet1_row_counter = 2
@@ -188,27 +188,27 @@ def build_GR() -> None:
             message(__name__, "SN NOT FOUND")
             continue
         # get the row, change SN to red
-        GR_FILE_SHEET2.Cells(res.Row, 5).Font.Color = 255
-        GR_FILE_SHEET2.Rows(res.Row).Copy()
-        GR_FILE_SHEET1.Rows(sheet1_row_counter).PasteSpecial(Paste=-4163)
-        GR_FILE_SHEET1.Rows(sheet1_row_counter).NumberFormat = "0"
+        gr_file_sheet2.Cells(res.Row, 5).Font.Color = 255
+        gr_file_sheet2.Rows(res.Row).Copy()
+        gr_file_sheet1.Rows(sheet1_row_counter).PasteSpecial(Paste=-4163)
+        gr_file_sheet1.Rows(sheet1_row_counter).NumberFormat = "0"
         sheet1_row_counter += 1
 
     message(__name__, "PROCESSING COMPLETE, FINALIZING GR FILE")
 
     # delete COL ret if exist
-    GR_FILE_SHEET1.Columns(16).Delete()
+    gr_file_sheet1.Columns(16).Delete()
 
     now = datetime.now()
     today_date = now.strftime("%m")+now.strftime("%d")
     # file name to save
-    final_GR_file_name = r"{}{} {}_{}x_{}.xlsx".format(GR_FILE_BASE_DIRECTORY, today_date, GR_PB, str(
+    final_GR_file_name = r"{}{} {}_{}x_{}.xlsx".format(gr_file_base_dir, today_date, GR_PB, str(
         len(sn_list)), CONFIG["GR"]["invoice_header"]+str(CONFIG["GR"]["invoice_record"]).rjust(4, '0'))
 
     # save data
-    GR_FILE_WORKBOOK.Save()
-    GR_FILE_WORKBOOK.Close(SaveChanges=False)
-    GR_FILE.Quit()
+    gr_file_workbook.Save()
+    gr_file_workbook.Close(False)
+    gr_file.Quit()
 
     # save next invoice
     CONFIG["GR"]["invoice_record"] = CONFIG["GR"]["invoice_record"]+1
@@ -219,3 +219,6 @@ def build_GR() -> None:
 
     message(__name__, "GR FILE BUILDING COMPLETE, FIEL CREATE: {}".format(
         final_GR_file_name))
+
+    # now go to do the GR on NV computer
+    exit(0)
